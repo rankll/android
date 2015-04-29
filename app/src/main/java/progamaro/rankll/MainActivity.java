3c3f1c74a63bb50524b9e503e1f98644eddde88d
andremiranda <andremirannda@gmail.com>
2015-04-28 21:02:50 -0300

    testando

--- app/src/main/java/progamaro/rankll/MainActivity.java
+++ app/src/main/java/progamaro/rankll/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("Teste", "Rankll ");
+        Log.i("Teste", "Rankll");
     }
 
 
